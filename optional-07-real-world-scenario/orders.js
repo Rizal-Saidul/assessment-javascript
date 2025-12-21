9bc52b19f9cd928016a96e6ce439a974fd967b77
Rizal_Saidul <muhamadsaidulrizal@gmail.com>
2025-12-21 23:38:24 +0700

    final ttest

--- optional-07-real-world-scenario/orders.js
+++ optional-07-real-world-scenario/orders.js
@@ -3,7 +3,6 @@ function generateUniqueId() {
   return `_${Math.random().toString(36).slice(2, 9)}`;
 }
 
-
 // TODO: buatlah variabel yang menampung data orders
 let orders = [];
 
@@ -15,14 +14,14 @@ function addOrder(customerName, items) {
     customerName: customerName,
     items: items,
     totalPrice: totalPrice,
-    status: 'Menunggu'
+    status: "Menunggu",
   };
   orders.push(newOrder);
 }
 
 // TODO: selesaikan fungsi updateOrderStatus
 function updateOrderStatus(orderId, status) {
-  const order = orders.find(o => o.id === orderId);
+  const order = orders.find((o) => o.id === orderId);
   if (order) {
     order.status = status;
   }
@@ -31,13 +30,19 @@ function updateOrderStatus(orderId, status) {
 // TODO: selesaikan fungsi calculateTotalRevenue dari order yang berstatus Selesai
 function calculateTotalRevenue() {
   return orders
-    .filter(order => order.status === 'Selesai')
+    .filter((order) => order.status === "Selesai")
     .reduce((sum, order) => sum + order.totalPrice, 0);
 }
 
 // TODO: selesaikan fungsi deleteOrder
 function deleteOrder(id) {
-  orders = orders.filter(order => order.id !== id);
+  orders = orders.filter((order) => order.id !== id);
 }
 
-export { orders, addOrder, updateOrderStatus, calculateTotalRevenue, deleteOrder };
+export {
+  orders,
+  addOrder,
+  updateOrderStatus,
+  calculateTotalRevenue,
+  deleteOrder,
+};
